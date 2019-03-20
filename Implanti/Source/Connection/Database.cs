7a1397abb0a4bc80b0bf6e462fcdd9d1384759c9
maiconkkl <ma_bonitao@hotmail.com>
2019-03-20 20:55:54 -0300

    Alterações
    ok

--- Implanti/Source/Connection/Database.cs
+++ Implanti/Source/Connection/Database.cs
@@ -10,8 +10,8 @@ namespace Implanti.Source
     class Database
     {
         private static IniFile myIni = new IniFile("Settings.ini");
-        private static readonly MongoClient _client = new MongoClient(myIni.Read("DatabaseUrl", "Settings"));
-        //private static readonly MongoClient _client = new MongoClient("mongodb://root:%7CcSFu%405rFv%23h8*%3D@localhost:12220/admin?readPreference=primary");
+        //private static readonly MongoClient _client = new MongoClient(myIni.Read("DatabaseUrl", "Settings"));
+        private static readonly MongoClient _client = new MongoClient("mongodb://root:%7CcSFu%405rFv%23h8*%3D@localhost:12220/admin?readPreference=primary");
 
         public static MongoClient GetConnection() => _client;        
     }
